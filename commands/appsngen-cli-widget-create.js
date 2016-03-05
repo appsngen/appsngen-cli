fb3ab74aaeea417669b0f1d8fd0fb67b54552db8
Alexander Bukley <alexander_bukley@epam.com>
2016-03-05 16:49:56 +0300

    Add generating to specific folder

--- commands/appsngen-cli-widget-create.js
+++ commands/appsngen-cli-widget-create.js
@@ -1,23 +1,48 @@
 #! /usr/bin/env node
 var cliConfig = require('../cli-config.json');
 var semver = require('semver');
+var readline = require('readline');
 var execSync = require('child_process').execSync;
+var fs = require('fs');
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 var installPackage = function (packageName) {
     execSync('npm install -g ' + packageName, {stdio: 'inherit'});
 };
-var info, packageInfo, dependency, packageVersion;
+var info, packageInfo, dependency, packageVersion, folderStat;
 
-for (dependency in cliConfig.dependencies) {
+rl.question('Enter project folder name: ', function (folder) {
+    rl.close();
     try {
-        info = execSync('npm list -g --depth=0 ' + dependency);
+        folderStat = fs.statSync('./' + folder);
     } catch (e) {
-        installPackage(dependency);
-        continue;
+        if (e.code !== 'ENOENT') {
+            throw e;
+        }
     }
-    packageInfo = info.toString().match(new RegExp(dependency + '@\\d\\.\\d\\.\\d', 'g'));
-    if (!packageInfo || !semver.satisfies(packageInfo[0].match(/\d\.\d\.\d/g)[0], cliConfig.dependencies[dependency])) {
-        installPackage(dependency);
+    if (folderStat && folderStat.isDirectory()) {
+        console.log('Folder with that name already exist.');
+        return;
     }
-    console.log(dependency + ' version check complete.');
-}
-execSync('yo appsngen-web-widget', {stdio: 'inherit'});
+    fs.mkdirSync('./' + folder);
+
+    for (dependency in cliConfig.dependencies) {
+        try {
+            info = execSync('npm list -g --depth=0 ' + dependency);
+        } catch (e) {
+            installPackage(dependency);
+            continue;
+        }
+        packageInfo = info.toString().match(new RegExp(dependency + '@\\d\\.\\d\\.\\d', 'g'));
+        if (!packageInfo || !semver.satisfies(packageInfo[0].match(/\d\.\d\.\d/g)[0], cliConfig.dependencies[dependency])) {
+            installPackage(dependency);
+        }
+        console.log(dependency + ' version check complete.');
+    }
+    execSync('yo appsngen-web-widget', {
+        cwd: './' + folder,
+        stdio: 'inherit'
+    });
+});
