24304aa6b6bf7f631f4f268102d3380a6f7c4e89
Alexander Bukley <alexander_bukley@epam.com>
2016-03-05 11:52:41 +0300

    Create command implementation

--- commands/appsngen-cli-widget-create.js
+++ commands/appsngen-cli-widget-create.js
@@ -1,3 +1,23 @@
 #! /usr/bin/env node
+var cliConfig = require('../cli-config.json');
+var semver = require('semver');
+var execSync = require('child_process').execSync;
+var installPackage = function (packageName) {
+    execSync('npm install -g ' + packageName, {stdio: 'inherit'});
+};
+var info, packageInfo, dependency, packageVersion;
 
-console.log('code to create widget. Run Yeoman generator');
+for (dependency in cliConfig.dependencies) {
+    try {
+        info = execSync('npm list -g --depth=0 ' + dependency);
+    } catch (e) {
+        installPackage(dependency);
+        continue;
+    }
+    packageInfo = info.toString().match(new RegExp(dependency + '@\\d\\.\\d\\.\\d', 'g'));
+    if (!packageInfo || !semver.satisfies(packageInfo[0].match(/\d\.\d\.\d/g)[0], cliConfig.dependencies[dependency])) {
+        installPackage(dependency);
+    }
+    console.log(dependency + ' version check complete.');
+}
+execSync('yo appsngen-web-widget', {stdio: 'inherit'});
