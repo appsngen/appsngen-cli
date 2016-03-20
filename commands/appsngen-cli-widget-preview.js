971cb5a7c673d5ffc59cb449304fa1c4ec4f24d2
Alexander Bukley <alexander_bukley@epam.com>
2016-03-20 13:41:24 +0300

    update script to install dependencies if they did not installed

--- commands/appsngen-cli-widget-preview.js
+++ commands/appsngen-cli-widget-preview.js
@@ -8,32 +8,51 @@ var execSync = require('child_process').execSync;
 var widgetPathw, devboxConfig, projectConfig, archiveName;
 var devboxPath = path.join(process.mainModule.paths[1], '/appsngen-dev-box');
 var devboxConfigPath = path.join(devboxPath, '/serverConfig.json');
+var errorHandler = function(error) {
+    console.error(error.toString());
+    process.exit(1);
+};
+var installDevBoxDependencies = function() {
+    console.log('Installing required dependencies...');
+    execSync('npm install', {
+        cwd: devboxPath,
+        stdio: 'inherit'
+    });
+};
 
 try {
     devboxConfig = jsonfile.readFileSync(devboxConfigPath);
     projectConfig = jsonfile.readFileSync('./.appsngenrc');
-    archiveName = path.basename(projectConfig.zipFilePath);
-    rmdir(path.join(devboxPath, '/widgets'), function(err) {
-        if (err) {
-            throw err;
-        }
-        fs.mkdirSync(path.join(devboxPath, '/widgets'));
-        fsExt.copySync(path.resolve(projectConfig.zipFilePath), path.join(devboxPath, '/widgets/', archiveName));
-        devboxConfig.widgets = [path.join('/widgets/', archiveName)];
-        jsonfile.writeFileSync(devboxConfigPath, devboxConfig, {
-            spaces: 4
-        });
-        //install devbox dependencies(routerhelpers.js require node_modules folder)
-        execSync('npm install', {
-            cwd: devboxPath,
-            stdio: 'inherit'
-        });
-        execSync('node server.js', {
-            cwd: devboxPath,
-            stdio: 'inherit'
-        });
-    });
 } catch (err) {
-    console.error(err.toString());
-    process.exit(1);
+    errorHandler(err);
 }
+
+archiveName = path.basename(projectConfig.zipFilePath);
+rmdir(path.join(devboxPath, '/widgets'), function(err) {
+    if (err) {
+        errorHandler(err);
+    }
+    fs.mkdirSync(path.join(devboxPath, '/widgets'));
+    fsExt.copySync(path.resolve(projectConfig.zipFilePath), path.join(devboxPath, '/widgets/', archiveName));
+    devboxConfig.widgets = [path.join('/widgets/', archiveName)];
+    jsonfile.writeFileSync(devboxConfigPath, devboxConfig, {
+        spaces: 4
+    });
+    //install devbox dependencies(routerhelpers.js require node_modules folder)
+    try {
+        if (!fs.statSync(path.join(devboxPath, '/node_modules')).isDirectory()) {
+            installDevBoxDependencies();
+        }
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            installDevBoxDependencies();
+        } else {
+            errorHandler(err);
+        }
+    }
+    execSync('node server.js', {
+        cwd: devboxPath,
+        stdio: 'inherit'
+    });
+});
+
