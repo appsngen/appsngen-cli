2b2b2127bcef6da15598533fa510060f1e245578
Alexander Bukley <alexander_bukley@epam.com>
2016-04-07 13:52:20 +0300

    Fix calls with widgets name

--- commands/appsngen-cli-login.js
+++ commands/appsngen-cli-login.js
@@ -8,18 +8,36 @@ authcontroller.authorize()
         var config;
         var configFilePath = path.join(__dirname, '/../cli-config.json');
 
-        if (response.statusCode === 201) {
-            config = jsonfile.readFileSync(configFilePath);
-            config.credentials = response.body;
-            config.credentials.received = Date.now();
-            jsonfile.writeFileSync(configFilePath, config, {
-                spaces: 4
-            });
-            console.log('Authorization completed successfully.');
-        } else {
-            console.log(response.body.message);
-            console.log('Unexpected response: ' + response.statusCode);
-            process.exit(1);
+        // if (response.statusCode === 201) {
+        //     config = jsonfile.readFileSync(configFilePath);
+        //     config.credentials = response.body;
+        //     config.credentials.received = Date.now();
+        //     jsonfile.writeFileSync(configFilePath, config, {
+        //         spaces: 4
+        //     });
+        //     console.log('Authorization completed successfully.');
+        // } else {
+        //     console.log(response.body.message);
+        //     console.log('Unexpected response: ' + response.statusCode);
+        //     process.exit(1);
+        // }
+        switch (response.statusCode) {
+            case 201:
+                config = jsonfile.readFileSync(configFilePath);
+                config.credentials = response.body;
+                config.credentials.received = Date.now();
+                jsonfile.writeFileSync(configFilePath, config, {
+                    spaces: 4
+                });
+                console.log('Authorization completed successfully.');
+                break;
+            case 401:
+                console.log('Invalid username or password.');
+                process.exit(1);
+            default:
+                console.log(response.body.message);
+                console.log('Unexpected response: ' + response.statusCode);
+                process.exit(1);
         }
     })
     .catch(function(error) {
