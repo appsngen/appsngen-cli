56559247d304d6588e73853f782fb5be8d4b2e00
Alexander Bukley <bukerok@gmail.com>
2016-04-26 21:40:44 +0300

    Update requests methods

--- commands/appsngen-phonegap-access.js
+++ commands/appsngen-phonegap-access.js
@@ -10,28 +10,29 @@
     var config = require('./../cli-config.json');
 
     var authToken;
-    var serviceAddress = config.serviceAddress + '/viewer/api/v1/phonegap-access-token';
     var requestOptions = {
-        url: serviceAddress + (authToken ? '?phonegap-auth-token=' + authToken : ''),
-        method: 'GET',
-        headers: [
-            {
-                name: 'content-type',
-                value: 'application/json'
-            },
-            {
-                name: 'Authorization',
-                value: 'Bearer ' + authcontroller.getIdentityToken()
-            }
-        ]
+        url: config.serviceAddress + '/viewer/api/v1/phonegap-access-token',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + authcontroller.getIdentityToken()
+        }
     };
 
     program
-    .arguments('[token]')
-    .action(function (token) {
-        authToken = token;
-    })
-    .parse(process.argv);
+        .arguments('[token]')
+        .action(function (token) {
+            authToken = token;
+        })
+        .parse(process.argv);
+
+    if (typeof authToken !== 'undefined') {
+        requestOptions.method = 'POST';
+        requestOptions.body = JSON.stringify({
+            'phonegap-auth-token': authToken
+        });
+    } else {
+        requestOptions.method = 'GET';
+    }
 
     request(requestOptions)
     .then(function (response) {
@@ -40,23 +41,26 @@
 
         switch (response.statusCode) {
             case 200:
-            config = jsonfile.readFileSync(configFilePath);
-            config.credentials.phonegap = response.body;
-            jsonfile.writeFileSync(configFilePath, config, {
-                spaces: 4
-            });
-            console.log('Access token received.');
-            break;
+            case 201:
+                config = jsonfile.readFileSync(configFilePath);
+                config.credentials.phonegap = JSON.parse(response.body);
+                jsonfile.writeFileSync(configFilePath, config, {
+                    spaces: 4
+                });
+                console.log('Access token received.');
+                break;
             case 404:
-            console.log('No access token for specified user. Use "appsngen phonegap access [authToken]" to get one.' +
-                'For instaructions how to get PhoneGap Authentication Token folow the link:' +
-                'https://github.com/appsngen/developers-documentation/wiki/How-to-get-PhoneGap-Authentication-Tokens');
-            process.exit(1);
-            break;
+                console.log('No access token for specified user.\n' +
+                            'Use "appsngen phonegap access [authToken]" to get one.\n' +
+                            'For instaructions how to get PhoneGap Authentication Token folow the link:\n' +
+                            'https://github.com/appsngen/developers-documentation/' +
+                            'wiki/How-to-get-PhoneGap-Authentication-Tokens');
+                process.exit(1);
+                break;
             default:
-            console.log(response.body.message);
-            console.log('Unexpected response: ' + response.statusCode);
-            process.exit(1);
+                console.log('Unexpected response: ' + response.statusCode);
+                console.log(response.body);
+                process.exit(1);
         }
     })
     .catch(function (err) {
