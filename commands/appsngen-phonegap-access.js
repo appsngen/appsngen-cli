10200f4fd2773ef311e581eb1da88fb3c19155f8
bukerok <bukerok@users.noreply.github.com>
2016-04-28 14:23:02 +0300

    Change processing of unexpected statuses

--- commands/appsngen-phonegap-access.js
+++ commands/appsngen-phonegap-access.js
@@ -64,14 +64,10 @@
                             'wiki/How-to-get-PhoneGap-Authentication-Tokens');
                 process.exit(1);
                 break;
-            case 500:
+            default:
                 console.log('Unexpected response from backend. Please try again.');
                 process.exit(1);
                 break;
-            default:
-                console.log('Unexpected response: ' + response.statusCode);
-                console.log(response.body);
-                process.exit(1);
         }
     })
     .catch(function (err) {
